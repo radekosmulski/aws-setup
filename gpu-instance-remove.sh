c93bb30f9c66df7a7e5bc98593575e3f0e6b3d30
Radek Osmulski <radoslaw.osmulski@radarservices.com>
2017-01-29 06:57:16 +0100

    Modify .gitignore
    Modify .gitignore to not include generated files specific to current AWS
    instantiation.

--- gpu-instance-remove.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws ec2 disassociate-address --association-id eipassoc-be67b3c6
-aws ec2 release-address --allocation-id eipalloc-12323c76
-aws ec2 terminate-instances --instance-ids i-0847435d66bf859ff
-aws ec2 wait instance-terminated --instance-ids i-0847435d66bf859ff
-echo If you want to delete the key-pair, please do it manually.
